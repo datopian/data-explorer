6c80af419d15a863ee231d4e1a73f7df7b1c5631
starsinmypockets <starsinmypockets@gmail.com>
2019-08-12 14:00:12 -0400

    [setup][m]: Tailwind CSS

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store'
-import './index.css'
+import './App.css'
 import App from './App'
 import { register } from './serviceWorker';
 
