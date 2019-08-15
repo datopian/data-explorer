089413165dc0926bfc8033687103083ad506ac29
anuveyatsu <anuar.ustayev@gmail.com>
2019-08-15 17:51:02 +0600

    [build][xl]

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import { Provider } from 'react-redux'
 import configureStore from './store'
 import './App.css'
 import App from './App'
-import { register } from './serviceWorker'
+import * as serviceWorker from './serviceWorker';
 
-// Mock init data
-import datapackage from './testData/remoteData.json'
+function parseDatapackageIdentifier(stringOrJSON) {
+  try {
+    return JSON.parse(stringOrJSON)
+  } catch (e) {
+    console.error(e)
+    return stringOrJSON
+  }
+}
+const datapackage = parseDatapackageIdentifier(window.DP_ID)
+
 
 ReactDOM.render(
  <Provider store={configureStore({sharedState: {datapackage}})}>
   <App />
  </Provider>,
- document.getElementById('root')
+ document.getElementById('data-explorer')
 )
 
-register()
+serviceWorker.unregister();
