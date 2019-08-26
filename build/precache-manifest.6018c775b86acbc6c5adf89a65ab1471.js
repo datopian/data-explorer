73c3b45eaf6fd28348aaa78ad62371344d4e6f1c
Anuar Ustayev <anuar.ustayev@gmail.com>
2019-08-26 11:03:49 +0000

    Integrated Map Renderer and Builder

---
+++ build/precache-manifest.6018c775b86acbc6c5adf89a65ab1471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5829afc29a760044ce3204651c1303cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fea2c13296d71509809",
+    "url": "/static/css/2.ceaf15e7.chunk.css"
+  },
+  {
+    "revision": "a44f1e07630a5e73e6d2",
+    "url": "/static/css/main.2af79e8e.chunk.css"
+  },
+  {
+    "revision": "0fea2c13296d71509809",
+    "url": "/static/js/2.b1be5051.chunk.js"
+  },
+  {
+    "revision": "a44f1e07630a5e73e6d2",
+    "url": "/static/js/main.1d321772.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/reducers/dataViewBuilderReducer.js
+++ src/reducers/dataViewBuilderReducer.js
@@ -1,6 +1,6 @@
 export default (state = {}, action) => {
  switch (action.type) {
-  case 'DATA_VIEW_BUILDER_ACTION':
+  case 'DATA_VIEW_CHART_BUILDER_ACTION':
     return state
   default:
     return state
