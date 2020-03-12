3a6ccc98001d4cb13102ec9b9f17fabfc2379e79
Sagar Ghimire <info@sagarg.com.np>
2020-03-12 18:09:30 +0545

    WIP: guiding text displays when chart/map views are blanked (#9)
    * [s]: guided message displayed when chart/map views are blank
    
    * [xs]: guiding text updated and unique css class added
    
    * [xs]: missing space added

--- src/App.js
+++ src/App.js
@@ -42,6 +42,15 @@ export const App = props => {
   const tabLinks = props.widgets.map((widget, index) => {
     return <TabLink to={widget.name} className='mr-4' key={`tabLink-${index}`}>{widget.name}</TabLink>
   })
+
+  const viewGuidingText = (specType) => {
+    return (
+    <div className="dx-guiding-text"> 
+      {specType === 'simple' ? <p>Select chart type, group column (ordinate x-axis) and series (abscissa y-axis) on the right hand side panel.</p> : '' }  
+      {specType === 'tabularmap' ? <p>Select geo data field on the right hand side panel.</p> : '' }  
+    </div>
+    )}
+
   const tabContents = props.widgets.map((widget, index) => {
     return <TabContent for={widget.name} key={`tabContent-${index}`}>
         {
@@ -53,12 +62,13 @@ export const App = props => {
             </div>
           : <div className="container flex py-6">
               <div className="w-3/4 py-3 mr-4">
-                <DataView {...widget} />
+              {!widget.datapackage.views[0].spec ? <div>{viewGuidingText(widget.datapackage.views[0].specType)}</div> : ''}
+              <DataView {...widget} />
               </div>
               <div className="w-1/4">
                 <div className="w-full">
                   <div className="p-4 mr-4">
-                    {
+                   {
                       widget.datapackage.views[0].specType === 'simple'
                       ? <ChartBuilder view={widget.datapackage.views[0]} dataViewBuilderAction={props.dataViewBuilderAction} />
                       : ''
