220dcdccfeda2548027f8873642617df79cf38d8
anuveyatsu <anuar.ustayev@gmail.com>
2020-03-13 10:40:09 +0600

    [guide text][s]: render guide text for chart component and only if spec is missing.

--- src/components/DataView/index.js
+++ src/components/DataView/index.js
@@ -5,12 +5,30 @@ import { DataView } from '@datopian/datapackage-views-js'
 export default props => {
   const views = props.datapackage.views
 
+  const showGuideText = (specType) => {
+    return (
+      <div className="dx-guiding-text">
+        {specType === 'simple' ? <p>Select chart type, group column (ordinate x-axis) and series (abscissa y-axis) on the right hand side panel.</p> : '' }
+        {specType === 'tabularmap' ? <p>Select geo data column on the right hand side panel.</p> : '' }
+      </div>
+    )
+  }
+
+  const checkIfGuideIsNeeded = (view) => {
+    if (view.specType === 'simple' && !(view.spec && Object.keys(view.spec).length > 0)) {
+      return true
+    }
+    return false
+  }
+
   return (
   <Loader loaded={!props.loading} style={{position: "relative"}}>
     <div>
-     {
-        views.map(view => <DataView key={Math.random()} datapackage={{views: [view]}} />)
-     }
+      {
+        checkIfGuideIsNeeded(views[0])
+        ? showGuideText(views[0].specType)
+        : views.map(view => <DataView key={Math.random()} datapackage={{views: [view]}} />)
+      }
     </div>
   </Loader>
   )
