78667b645186735e64078d19a9583626e76e8f2a
anuveyatsu <anuar.ustayev@gmail.com>
2020-03-13 13:42:29 +0600

    v0.4.8

--- dist/components/DataView/index.js
+++ dist/components/DataView/index.js
@@ -15,12 +15,27 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var _default = function _default(props) {
   var views = props.datapackage.views;
+
+  var showGuideText = function showGuideText(specType) {
+    return _react.default.createElement("div", {
+      className: "dx-guiding-text"
+    }, specType === 'simple' ? _react.default.createElement("p", null, "Select chart type, group column (ordinate x-axis) and series (abscissa y-axis) on the right hand side panel.") : '', specType === 'tabularmap' ? _react.default.createElement("p", null, "Select geo data column on the right hand side panel.") : '');
+  };
+
+  var checkIfGuideIsNeeded = function checkIfGuideIsNeeded(view) {
+    if (view.specType === 'simple' && !(view.spec && Object.keys(view.spec).length > 0)) {
+      return true;
+    }
+
+    return false;
+  };
+
   return _react.default.createElement(_reactLoader.default, {
     loaded: !props.loading,
     style: {
       position: "relative"
     }
-  }, _react.default.createElement("div", null, views.map(function (view) {
+  }, _react.default.createElement("div", null, checkIfGuideIsNeeded(views[0]) ? showGuideText(views[0].specType) : views.map(function (view) {
     return _react.default.createElement(_datapackageViewsJs.DataView, {
       key: Math.random(),
       datapackage: {
