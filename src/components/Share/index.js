3d7389eb5ed32c648b15fd327e8cd1be7f1e3d0d
Michael Polidori <spatiumestabsolutum@gmail.com>
2020-12-03 09:27:56 -0500

    [WCAG][xs]: Adds dx-share-link to share links div class and add missing dashes to IDs

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -48,20 +48,20 @@ export default props => {
       <div className="dx-share-container">
         {shareable
           ? <div>
-              <div className="m-4 ml-0">
+              <div className="m-4 ml-0 dx-share-link">
                 <input id={"share-link-"+Math.random().toString(36).slice(2,5)} title="Share link" className="border-solid border-2 border-gray-600 w-1/2 px-2" value={shareLink} />
                 <a href="#/" id={"copy-share-link-"+Math.random().toString(36).slice(2,5)} className="m-4" onClick={() => {copy(shareLink)}}><i>{t("copy share link")}</i></a>
               </div>
-              <div className="m-4 ml-0">
-                <input id={"embed"+Math.random().toString(36).slice(2,5)} title="Embedded link" className="border-solid border-2 border-gray-600 px-2 w-1/2" value={iframe} />
+              <div className="m-4 ml-0 dx-share-link">
+                <input id={"embed-"+Math.random().toString(36).slice(2,5)} title="Embedded link" className="border-solid border-2 border-gray-600 px-2 w-1/2" value={iframe} />
                 <a href="#/" id={"copy-share-link-"+Math.random().toString(36).slice(2,5)} className="m-4" onClick={() => {copy(iframe)}}><i>{t("copy embed text")}</i></a>
               </div>
             </div>
           : <p className="no-share-link-message">{t('No share link available')}</p>
         }
         {props.apiUri
-          && <div className="m-4 ml-0">
-                <input id={"apiUri"+Math.random().toString(36).slice(2,5)} title="API URI link" className="border-solid border-2 border-gray-600 px-2 w-1/2" value={props.apiUri} />
+          && <div className="m-4 ml-0 dx-share-link">
+                <input id={"apiUri-"+Math.random().toString(36).slice(2,5)} title="API URI link" className="border-solid border-2 border-gray-600 px-2 w-1/2" value={props.apiUri} />
                 <a href="#/" id={"copy-share-link-"+Math.random().toString(36).slice(2,5)} className="m-4" onClick={() => {copy(props.apiUri)}}><i>{t("copy API URI")}</i></a>
              </div>
         }
