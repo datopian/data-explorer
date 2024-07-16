3861fc2bec59a18c8b82030faedf9506fc4aeba3
Sagar Ghimire <info@sagarg.com.np>
2024-07-16 09:45:29 +0545

    fix: improve share link fucntion to prevent from possible XSS security issue

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -27,21 +27,8 @@ export default props => {
   const iframe = `<iframe src="${urlObj.href}" />`
   const shareable = shareLink.length < 2000
 
-  const copy = (str) => {
-    // Create new element
-    var el = document.createElement('textarea')
-    // Set value (string to be copied)
-    el.value = str
-    // Set non-editable to avoid focus and move outside of view
-    el.setAttribute('readonly', '')
-    el.style = {position: 'absolute', left: '-9999px'}
-    document.body.appendChild(el)
-    // Select text inside element
-    el.select()
-    // Copy text to clipboard
-    document.execCommand('copy')
-    // Remove temporary element
-    document.body.removeChild(el)
+  const copy = async (str) => {
+    await navigator.clipboard.writeText(str);
   }
 
   return (
